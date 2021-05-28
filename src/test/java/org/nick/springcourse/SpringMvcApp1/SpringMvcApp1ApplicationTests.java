347ef4a3979313b29e71671d9085a05116e310eb
DeliriumTraemens <oskinnick@gmail.com>
2021-05-28 14:48:59 +0300

    Initial commit

---
+++ src/test/java/org/nick/springcourse/SpringMvcApp1/SpringMvcApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nick.springcourse.SpringMvcApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
