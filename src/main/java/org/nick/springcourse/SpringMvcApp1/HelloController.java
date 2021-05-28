75e5e9f7190268ebfc617be4b0e11b870c41afa2
DeliriumTraemens <oskinnick@gmail.com>
2021-05-28 15:08:21 +0300

    First Step - Hello world

---
+++ src/main/java/org/nick/springcourse/SpringMvcApp1/HelloController.java
@@ -0,0 +1,13 @@
+package org.nick.springcourse.SpringMvcApp1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello-world")
+	public String sayhello(){
+		return "hello_world";
+	}
+}
